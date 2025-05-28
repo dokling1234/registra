97c2b963e70a55bd847344fd8a698f0f71363ea1
dokling1234 <carrel.golosinda@gmail.com>
2025-05-28 11:29:04 +0800

    fixed bugs

--- server/services/user.services.js
+++ server/services/user.services.js
@@ -1,76 +1,76 @@
 
-// const userModel = require('../models/userModel.js');
-// const jwt = require('jsonwebtoken');
+const userModel = require('../models/userModel.js');
+const jwt = require('jsonwebtoken');
 
-// class UserService {
-//   static async registerUser(
-//     fullName,
-//     contactNumber,
-//     email,
-//     hashedPassword,
-//     confirmPassword,
-//     icpepId,
-//     age,
-//     userType,
-//     membership,
-//     aboutMe = "",
-//     profileImage = ""
-//   ) {
-//     try {
-//       const createUser = new userModel({
-//         fullName,
-//         contactNumber,
-//         email,
-//         password: hashedPassword,
-//         confirmPassword,
-//         icpepId,
-//         age,
-//         userType,
-//         membership,
-//         aboutMe,
-//         profileImage,
-//       });
-//       return await createUser.save();
-//     } catch (err) {
-//       throw err;
-//     }
-//   }
+class UserService {
+  static async registerUser(
+    fullName,
+    contactNumber,
+    email,
+    hashedPassword,
+    confirmPassword,
+    icpepId,
+    age,
+    userType,
+    membership,
+    aboutMe = "",
+    profileImage = ""
+  ) {
+    try {
+      const createUser = new userModel({
+        fullName,
+        contactNumber,
+        email,
+        password: hashedPassword,
+        confirmPassword,
+        icpepId,
+        age,
+        userType,
+        membership,
+        aboutMe,
+        profileImage,
+      });
+      return await createUser.save();
+    } catch (err) {
+      throw err;
+    }
+  }
 
-//   static async checkuser(email) {
-//     try {
-//       return await userModel.findOne({ email });
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
+  static async checkuser(email) {
+    try {
+      return await userModel.findOne({ email });
+    } catch (error) {
+      throw error;
+    }
+  }
 
-//   static async updateUser(email, hashedPassword) {
-//     try {
-//       const result = await userModel.updateOne(
-//         { email },
-//         { $set: { password: hashedPassword } }
-//       );
-//       return result;
-//     } catch (err) {
-//       throw err;
-//     }
-//   }
+  static async updateUser(email, hashedPassword) {
+    try {
+      const result = await userModel.updateOne(
+        { email },
+        { $set: { password: hashedPassword } }
+      );
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
 
-//   static async generateToken(tokenData, secretKey, jwt_expire) {
-//     return jwt.sign(tokenData, secretKey, { expiresIn: jwt_expire });
-//   }
+  static async generateToken(tokenData, secretKey, jwt_expire) {
+    return jwt.sign(tokenData, secretKey, { expiresIn: jwt_expire });
+  }
 
-//   static async verifyUser(email, isVerified) {
-//     try {
-//       const result = await userModel.updateOne(
-//         { email },
-//         { $set: { isVerified: true } }
-//       );
-//       return result;
-//     } catch (err) {
-//       throw err;
-//     }
-//   }
-// }
+  static async verifyUser(email, isVerified) {
+    try {
+      const result = await userModel.updateOne(
+        { email },
+        { $set: { isVerified: true } }
+      );
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+}
 
-// module.exports = UserService;
+module.exports = UserService;
