da4571a62fc778acbca8b72ae97ae7af3bac7df6
dokling1234 <carrel.golosinda@gmail.com>
2025-08-15 18:08:00 +0800

    aug15 updates

--- server/routes/feedbackRoutes.js
+++ server/routes/feedbackRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getFeedbackForm, createFeedbackForm, submitFeedback, checkSubmission, mobileSubmitFeedback, mobileCheckSubmission, getEventFeedbackData} = require('../controllers/feedbackController.js');
+const { getFeedbackForm, createFeedbackForm, submitFeedback, checkSubmission, mobileSubmitFeedback, mobileCheckSubmission, getEventFeedbackData } = require('../controllers/feedbackController.js');
 const userAuth = require('../middleware/userAuth.js');
 
 const feedbackRoutes = express.Router();
@@ -9,7 +9,6 @@ feedbackRoutes.put('/createFeedback', createFeedbackForm);
 feedbackRoutes.post('/submitFeedback/:formId',userAuth, submitFeedback); // formId
 feedbackRoutes.get('/checkSubmission/:eventId', userAuth, checkSubmission);//eventId
 feedbackRoutes.get('/getEventFeedbackData/:eventId', getEventFeedbackData); //eventId
-// feedbackRoutes.get('/feedback/summary/:formId', analyzeFeedback); //eventId
 
 
 const mobileFeedbackRoutes = express.Router();
